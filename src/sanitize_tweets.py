cccdd2e6fddfbb734ce56bf505d3a6a40ffe17c6
Ahmed AlTaha <ahmedsalam.2008@gmail.com>
2015-11-06 07:16:04 +0000

    Unit test 1 passing

--- src/sanitize_tweets.py
+++ src/sanitize_tweets.py
@@ -1,10 +1,28 @@
+import json
+
 class TweetSanitizer(object):
     def __init__(self):
         self.tweets_with_unicode = 0
 
-    def sanitize_tweet(self, line):
-        pass
+    def sanitize_tweet(self, json_tweet):
+        return self._load_json_tweet(json_tweet)
 
-    def get_num_tweets_with_unicode(self):
+    def num_tweets_with_unicode(self):
         return self.tweets_with_unicode
 
+    def _load_json_tweet(self, json_tweet):
+        tweet = json.loads(json_tweet)
+        text = tweet['text']
+        created_at = tweet['created_at']
+        return Tweet(text, created_at)
+
+class Tweet(object):
+    def __init__(self, text, created_at):
+        self.text = text
+        self.created_at = created_at
+
+    def __str__(self):
+        return '{text} (timestamp: {created_at})'.format(
+            text=self.text,
+            created_at=self.created_at,
+        )
--- src/test_sanitize_tweets.py
+++ src/test_sanitize_tweets.py
@@ -1,23 +1,27 @@
+import json
 import unittest
 
 from sanitize_tweets import TweetSanitizer
 
-test_json = (
+json_tweet = (
     '{"created_at":"Thu Oct 29 17:51:01 +0000 2015", "id":659789759787589632,'
     '"id_str":"659789759787589632", "text":"Spark Summit East this week! #Spark #Apache"}'
 )
 
-test_unicode = (
-    '{"created_at":"Thu Oct 29 18:10:49 +0000 2015", "text":"Im at Terminal de Integra\u00e7\u00e3o do Varadouro in Jo\u00e3o Pessoa"}'
-)
+unicode_tweet = {
+    "created_at": "Thu Oct 29 18:10:49 +0000 2015",
+    "text": unicode("I'm at Terminal de Integra\u00e7\u00e3o do Varadouro in Jo\u00e3o Pessoa", encoding='utf_8')
+}
 
-test_escaped = (
-    '{"created_at":"Thu Oct 29 18:10:49 +0000 2015", "text":", PB https:\/\/t.co\/HOl34REL1a"}'
-)
+escaped_tweet = {
+    "created_at": "Thu Oct 30 18:10:49 +0000 2015",
+    "text": ", PB https:\/\/t.co\/HOl34REL1a"
+}
 
-all_escape_chars = (
-    '{"  \/Hello \\ \' \" n\no   di\tce "}'
-)
+all_escaped_tweet = {
+    "created_at": "Fri Oct 30 18:10:49 +0000 2015",
+    "text": "  \/Hello \\ \' \" n\no   di\tce "
+}
 
 class TestSanitizer(unittest.TestCase):
     def setUp(self):
@@ -26,25 +30,28 @@ class TestSanitizer(unittest.TestCase):
     def test_load_json(self):
         """Loads basic JSON tweet that doesnt require sanitization"""
         expected_output = 'Spark Summit East this week! #Spark #Apache (timestamp: Thu Oct 29 17:51:01 +0000 2015)'
-        sanitized_output = self.sanitizer.sanitize_tweet(test_unicode)
+        sanitized_output = str(self.sanitizer.sanitize_tweet(json_tweet))
         self.assertEqual(expected_output, sanitized_output)
 
     def test_remove_unicode(self):
+        tweet = json.dumps(unicode_tweet)
         expected_output = 'Im at Terminal de Integrao do Varadouro in Joo Pessoa (timestamp: Thu Oct 29 18:10:49 +0000 2015)'
-        sanitized_output = self.sanitizer.sanitize_tweet(test_unicode)
+        sanitized_output = str(self.sanitizer.sanitize_tweet(tweet))
         self.assertEqual(expected_output, sanitized_output)
 
-        sanitized_output = self.sanitizer.sanitize_tweet(test_unicode)
-        num_unicode = self.sanitizer.num_with_unicode()
+        sanitized_output = self.sanitizer.sanitize_tweet(tweet)
+        num_unicode = self.sanitizer.num_tweets_with_unicode()
         self.assertEqual(num_unicode, 2)
 
     def test_unescape_text(self):
-        expected_output = ', PB https://t.co/HOl34REL1a (timestamp: Thu Oct 29 18:10:49 +0000 2015)'
-        sanitized_output = self.sanitizer.sanitize_tweet(test_escaped)
+        tweet = json.dumps(escaped_tweet)
+        expected_output = ', PB https://t.co/HOl34REL1a (timestamp: Thu Oct 30 18:10:49 +0000 2015)'
+        sanitized_output = str(self.sanitizer.sanitize_tweet(tweet))
         self.assertEqual(expected_output, sanitized_output)
 
     def test_all_escape_charecters(self):
+        tweet = json.dumps(all_escaped_tweet)
+        print tweet
         expected_output = '/Hellow \ \' " n o di ce (timestamp: Wed Oct 29 18:10:49 +0000 2015)'
-        sanitized_output = self.sanitizer.sanitize_tweet(all_escape_chars)
+        sanitized_output = str(self.sanitizer.sanitize_tweet(tweet))
         self.assertEqual(expected_output, sanitized_output)
-        pass
