4ff36d451749d45762f17486b515eefca925aaa9
Ahmed AlTaha <ahmedsalam.2008@gmail.com>
2015-11-06 22:51:23 -0500

    Add CLI options to feature 1

--- src/sanitize_tweets.py
+++ src/sanitize_tweets.py
@@ -1,4 +1,6 @@
+import getopt
 import json
+import sys
 
 escaping_map = [
     ('\/', '/',),
@@ -59,14 +61,14 @@ class Tweet(object):
 
 
 ## Feature 1 execution script ##
-sanitizer = None
 tweets_out = None
 
 def write_line(line):
     tweets_out.write(line + '\n')
 
-def sanitize_all_tweets():
-    with open('../data-gen/tweets.txt') as tweets_in:
+def sanitize_all_tweets(input_file):
+    sanitizer = TweetSanitizer()
+    with open(input_file) as tweets_in:
         for line in tweets_in:
             tweet = sanitizer.sanitize_tweet(line)
             write_line(str(tweet))
@@ -76,8 +78,24 @@ def sanitize_all_tweets():
         write_line('{0} tweets contained unicode.'.format(num_unicode))
 
 
-if __name__ == '__main__':
-    sanitizer = TweetSanitizer()
-    tweets_out = open('../tweet_output/ft1.txt', 'w')
-    sanitize_all_tweets()
+def main(argv):
+    input_file = '../data-gen/tweets.txt'
+    output_file = '../tweet_output/ft1.txt'
+    try:
+        (opts, args) = getopt.getopt(argv, 'i:o:')
+    except getopt.GetoptError:
+        print 'sanitize_tweets.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-i'):
+            input_file = arg
+        elif opt in ('-o'):
+            output_file = arg
+
+    global tweets_out
+    tweets_out = open(output_file, 'w')
+    sanitize_all_tweets(input_file)
     tweets_out.close()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
